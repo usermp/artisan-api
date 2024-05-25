5180d8a997f8971ea45301b39dc119e19bd9b4cd
mohammad <usermp76@gmail.com>
2024-05-25 15:37:45 +0330

    Add exception

--- src/app/Http/Controllers/ArtisanController.php
+++ src/app/Http/Controllers/ArtisanController.php
@@ -5,16 +5,34 @@ namespace Usermp\ArtisanApi\app\Http\Controllers;
 use Illuminate\Routing\Controller; 
 use Illuminate\Support\Facades\Artisan;
 use Usermp\ArtisanApi\app\Http\Requests\ArtisanRequest;
+use Illuminate\Http\JsonResponse;
 
 class ArtisanController extends Controller
 {
-    public function store(ArtisanRequest $request)
+    public function store(ArtisanRequest $request): JsonResponse
     {
         $validated = $request->validated();
-        if(config("artisan-api-config.with_key"))
-        {
-            Artisan::call($validated['command']);
-            return Artisan::output();
+        
+        if (config("artisan-api-config.with_key")) {
+            try {
+                Artisan::call($validated['command']);
+                $output = Artisan::output();
+                
+                return response()->json([
+                    'success' => true,
+                    'output' => $output
+                ], 200);
+            } catch (\Exception $e) {
+                return response()->json([
+                    'success' => false,
+                    'error' => $e->getMessage()
+                ], 500);
+            }
         }
+
+        return response()->json([
+            'success' => false,
+            'error' => 'Unauthorized access'
+        ], 403);
     }
 }
