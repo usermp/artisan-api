0e3a99fd753bf128f9648471a8567c7b5b155c32
mohammad <usermp76@gmail.com>
2024-05-25 14:34:20 +0330

    Add artisan class

--- src/app/Http/controllers/ArtisanController.php
+++ src/app/Http/controllers/ArtisanController.php
@@ -3,11 +3,15 @@
 namespace Usermp\ArtisanApi\app\Http\Controllers\Auth;
 
 use Illuminate\Routing\Controller; 
+use Illuminate\Support\Facades\Artisan;
 use Usermp\ArtisanApi\app\Http\Requests\Auth\ArtisanRequest;
 
 class ArtisanController extends Controller
 {
     public function store(ArtisanRequest $request)
     {
+        $validated = $request->validated();
+        Artisan::call($validated['command']);
+        return Artisan::output();
     }
 }
