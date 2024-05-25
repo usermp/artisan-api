e9cdf524eabfb47923a7d350c974ca16734a4e83
mohammad <usermp76@gmail.com>
2024-05-25 12:27:44 +0330

    Add artisan controller

---
+++ src/app/Http/controllers/ArtisanController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Usermp\ArtisanApi\app\Http\Controllers\Auth;
+
+use Illuminate\Routing\Controller; 
+
+class ArtisanController extends Controller
+{
+    public function store()
+    {
+    }
+}
